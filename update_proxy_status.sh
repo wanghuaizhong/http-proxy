4cc559c9212e2b2a1635d1c424408e3f07ec043f
wangzhiju <wangzhiju@@gu360.com>
2018-12-04 16:09:39 +0800

    init projects

--- update_proxy_status.sh
+++ update_proxy_status.sh
@@ -10,6 +10,6 @@ SQL_1="update proxys set status = -2 where status = -1 and valid_times = 0 and i
 SQL_2="update proxys set status = -2 where status = -1 and valid_times < 5 and invalid_times > 500 and last_valid_time < '$DATE_2'"
 SQL_3="update proxys set status = -2 where status = -1 and last_valid_time < '$DATE_3'"
 
-mysql -ustock -p360yahoo 91z -e "$SQL_1"
-mysql -ustock -p360yahoo 91z -e "$SQL_2"
-mysql -ustock -p360yahoo 91z -e "$SQL_3"
+mysql -utest -p123456 proxy -e "$SQL_1"
+mysql -utest -p123456 proxy -e "$SQL_2"
+mysql -utest -p123456 proxy -e "$SQL_3"
